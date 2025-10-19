1148721d471eecbe86efc0b3b4b44e7a1146bcb3
Rohit <rohitrk.singh1920@gmail.com>
2025-10-19 13:35:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (11.25%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,10 +1,20 @@
 class Solution {
     public int fib(int n) {
-        // using recursive approach
+        // using itrative approach
 
         if(n <= 1) {
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        
+        int first_term = 0;
+        int second_term = 1;
+
+        for(int i = 1; i<= n; i++) {
+            int third_term = first_term + second_term;
+
+            first_term = second_term;
+            second_term = third_term;
+        }
+        return first_term;
     }
 }
