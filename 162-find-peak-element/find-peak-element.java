7751265456f025da01030c197090220628d83023
Rohit <rohitrk.singh1920@gmail.com>
2025-11-05 17:50:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (5.05%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length == 1) {
+            return 0;
+        }
+        else if(nums[0] > nums[1]) {
+            return 0;
+        }
+        else if(nums[nums.length-1] > nums[nums.length-2]) {
+            return nums.length-1;
+        }
+        else {
+            int start = 1;
+            int end = nums.length-2;
+
+            while(start <= end) {
+                int mid = (start + end)/2;
+                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+                    return mid;
+                }
+                else if(nums[mid-1] > nums[mid]) {
+                    end = mid-1;
+                }
+                else {
+                    start = mid+1;
+                }
+            }
+            return -1;
+        }
+    }
+}
