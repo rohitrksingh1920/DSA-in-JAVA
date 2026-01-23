27e32122ea5e93ca8e2e1740fb59f0b7622d013e
Rohit <rohitrk.singh1920@gmail.com>
2026-01-23 19:50:57 +0530

    Time: 451 ms (97.47%) | Memory: 162.9 MB (93.04%) - LeetSync

---
+++ 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1) return 0;
+
+        vector<long long> val(n);
+        for (int i = 0; i < n; i++) val[i] = nums[i];
+
+        // Doubly linked list to track neighbors
+        vector<int> left(n), right(n);
+        for (int i = 0; i < n; i++) {
+            left[i] = i - 1;
+            right[i] = i + 1;
+        }
+        right[n - 1] = -1;
+
+        // Min-heap to store (sum, index of the left element of the pair)
+        using Pair = pair<long long, int>;
+        priority_queue<Pair, vector<Pair>, greater<Pair>> pq;
+        
+        int inversions = 0;
+        for (int i = 0; i < n - 1; i++) {
+            pq.push({val[i] + val[i + 1], i});
+            if (val[i] > val[i + 1]) inversions++;
+        }
+
+        int operations = 0;
+        vector<bool> removed(n, false);
+
+        while (inversions > 0) {
+            auto [current_sum, i] = pq.top();
+            pq.pop();
+
+            // Lazy deletion: skip if this pair is no longer valid
+            if (removed[i] || right[i] == -1 || val[i] + val[right[i]] != current_sum) {
+                continue;
+            }
+
+            int j = right[i];
+            int prev = left[i];
+            int next = right[j];
+
+            // Update inversions before merging
+            if (prev != -1 && val[prev] > val[i]) inversions--;
+            if (val[i] > val[j]) inversions--;
+            if (next != -1 && val[j] > val[next]) inversions--;
+
+            // Merge pair (i, j) into i
+            val[i] = current_sum;
+            removed[j] = true;
+            right[i] = next;
+            if (next != -1) left[next] = i;
+
+            // Update inversions after merging
+            if (prev != -1 && val[prev] > val[i]) inversions++;
+            if (next != -1 && val[i] > val[next]) inversions++;
+
+            // Push new potential pairs to PQ
+            if (prev != -1) pq.push({val[prev] + val[i], prev});
+            if (next != -1) pq.push({val[i] + val[next], i});
+
+            operations++;
+        }
+
+        return operations;
+    }
+};
