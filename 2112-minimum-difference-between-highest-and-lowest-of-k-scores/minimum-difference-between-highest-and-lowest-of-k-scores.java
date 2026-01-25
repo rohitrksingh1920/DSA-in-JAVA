bb3b0487a4598277062dca6db319e4ad79cdbe81
Rohit <rohitrk.singh1920@gmail.com>
2026-01-25 12:51:04 +0530

    Time: 8 ms (89.22%) | Memory: 46.9 MB (65.13%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.java
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -1,21 +1,38 @@
+// class Solution {
+//     public int minimumDifference(int[] nums, int k) {
+
+//         if(k == 1) {
+//             return 0;
+//         }
+
+//         Arrays.sort(nums);
+//         int minDiff = Integer.MAX_VALUE;
+
+//         for(int i = 0; i <= nums.length + k - 2; i++) {
+//             for(int j = i; j <= nums.length - 1; j++) {
+//                 if(j - i + 1 == k) {
+//                     minDiff = Math.min(minDiff, nums[j] - nums[i]);
+//                     break;
+//                 }
+//             }
+//         }
+//         return minDiff;
+//     }
+// }
+
 class Solution {
     public int minimumDifference(int[] nums, int k) {
-
-        if(k == 1) {
+        if (k == 1) {
             return 0;
         }
 
         Arrays.sort(nums);
         int minDiff = Integer.MAX_VALUE;
 
-        for(int i = 0; i <= nums.length + k - 2; i++) {
-            for(int j = i; j <= nums.length - 1; j++) {
-                if(j - i + 1 == k) {
-                    minDiff = Math.min(minDiff, nums[j] - nums[i]);
-                    break;
-                }
-            }
+        for (int i = 0; i + k - 1 <= nums.length - 1; i++) {
+            minDiff = Math.min(minDiff, nums[i + k - 1] - nums[i]);
         }
+
         return minDiff;
     }
 }
