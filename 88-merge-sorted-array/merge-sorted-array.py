6545a98ffe85a942eda791b1774cd2864bca5789
Rohit <rohitrk.singh1920@gmail.com>
2026-02-06 22:08:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (22.73%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,7 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
