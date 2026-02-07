0d1909e90bddce6847a7caa8e0f700607db70520
Rohit <rohitrk.singh1920@gmail.com>
2026-02-07 23:21:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.33%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,18 +4,40 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         zero = 0
-        curr = 0
-        two = len(nums) - 1
+        one = 0
+        two = 0
 
-        while curr <= two:
-            if nums[curr] == 0:
-                nums[zero], nums[curr] = nums[curr], nums[zero]
+        for val in nums:
+            if val == 0:
                 zero += 1
-                curr += 1
-
-            elif nums[curr] == 2:
-                nums[curr], nums[two] = nums[two], nums[curr]
-                two -= 1
-
+            elif val == 1:
+                one += 1
             else:
-                curr += 1
+                two += 1
+
+        for i in range(0, zero):
+            nums[i] = 0
+
+        for i in range(zero, zero + one):
+            nums[i] = 1
+
+        for i in range(zero + one, zero + one + two):
+            nums[i] = 2
+
+
+        # zero = 0
+        # curr = 0
+        # two = len(nums) - 1
+
+        # while curr <= two:
+        #     if nums[curr] == 0:
+        #         nums[zero], nums[curr] = nums[curr], nums[zero]
+        #         zero += 1
+        #         curr += 1
+
+        #     elif nums[curr] == 2:
+        #         nums[curr], nums[two] = nums[two], nums[curr]
+        #         two -= 1
+
+        #     else:
+        #         curr += 1
