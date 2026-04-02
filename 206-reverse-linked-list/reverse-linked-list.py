bbed12f23900472ddef8eae1ba1fb8403cc8c873
Rohit <rohitrk.singh1920@gmail.com>
2026-04-02 20:15:42 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (46.81%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -6,7 +6,7 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        if head is None:
+        if head == None:
             return
         
         stack = []
