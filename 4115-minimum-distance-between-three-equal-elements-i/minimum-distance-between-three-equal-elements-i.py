96e5d1d7cafca0360cb1125d80044a27a761b332
Rohit <rohitrk.singh1920@gmail.com>
2026-04-12 14:15:56 +0530

    Time: 7 ms (37.95%) | Memory: 19.3 MB (72.64%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -4,20 +4,22 @@ class Solution:
         n = len(nums)
         minDis = float('inf')
 
-        for i in range(n):
-            count = 1
-            prev = i
+        for j in range(n):
+            i = -1
+            k = -1
 
-            for j in range(i + 1, n):
-                if nums[j] == nums[i]:
-                    count += 1
+            for left in range(j - 1, -1, -1):
+                if nums[left] == nums[j]:
+                    i = left
+                    break
 
-                    if count == 2:
-                        mid = j
-                    elif count == 3:
-                        k = j
-                        dis = (mid - i) + (k - mid) + (k - i)
-                        minDis = min(minDis, dis)
-                        break
+            for right in range(j + 1, n):
+                if nums[right] == nums[j]:
+                    k = right
+                    break
+
+            if i != -1 and k != -1:
+                dis = (j - i) + (k - j) + (k - i)
+                minDis = min(minDis, dis)
 
         return minDis if minDis != float('inf') else -1
