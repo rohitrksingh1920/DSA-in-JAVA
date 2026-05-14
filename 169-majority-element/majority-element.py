7ed896823b73cca75416b90125ea3a2bc9630c66
Rohit <rohitrk.singh1920@gmail.com>
2026-05-14 20:31:20 +0530

    Time: 4 ms (71.13%) | Memory: 21.3 MB (20.83%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -13,13 +13,36 @@ class Solution:
         #         return i
         
 
-        freq = {}
+        # freq = {}
 
+        # for curr in nums:
+        #     if curr in freq:
+        #         freq[curr] += 1
+        #     else:
+        #         freq[curr] = 1
+
+        #     if freq[curr] > n // 2:
+        #         return curr
+
+        # count = 0
+        # candidate = 0
+        # for curr in nums:
+        #     if count == 0:
+        #         candidate = curr
+        #     if curr == candidate:
+        #         count += 1
+        #     count -= 1
+        # return candidate
+
+
+        count = 0
+        candidate = 0
         for curr in nums:
-            if curr in freq:
-                freq[curr] += 1
+            if count == 0:
+                candidate = curr
+            if curr == candidate:
+                count += 1
             else:
-                freq[curr] = 1
+                count -= 1
 
-            if freq[curr] > n // 2:
-                return curr
+        return candidate
