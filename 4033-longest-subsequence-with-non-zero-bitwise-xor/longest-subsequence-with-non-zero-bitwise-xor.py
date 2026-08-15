42cf30d01a1267367ad94a0a962b38534c8308fc
Rohit <rohitrk.singh1920@gmail.com>
2026-08-15 16:25:39 +0530

    Time: 38 ms (38.71%) | Memory: 34.2 MB (20.16%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.py
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.py
@@ -8,16 +8,10 @@ class Solution:
             if x != 0:
                 has_nonzero = True
 
-        # If total XOR is already non-zero,
-        # the whole array is the answer.
         if xor != 0:
             return len(nums)
 
-        # If all elements are zero, no subsequence
-        # can have non-zero XOR.
         if not has_nonzero:
             return 0
 
-        # Total XOR is zero, but there is at least
-        # one non-zero element. Remove one non-zero element.
         return len(nums) - 1
