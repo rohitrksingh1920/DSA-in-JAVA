a337aad03fc1004d758a070dd26b1e595bdc3d6f
Rohit <rohitrk.singh1920@gmail.com>
2026-09-07 12:06:34 +0530

    Time: 7 ms (71.86%) | Memory: 21.3 MB (11.66%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,23 +1,55 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # n = len(height)
+        # if n == 0:
+        #     return 0
+        # leftGre = [0] * n
+        # rightGre = [0] * n
 
-        n = len(height)
-        if n == 0:
-            return 0
+        # leftGre[0] = height[0]
+        # for i in range(1, n):
+        #     leftGre[i] = max(leftGre[i-1], height[i])
 
-        leftGre = [0] * n
-        rightGre = [0] * n
+        # rightGre[n-1] = height[n-1]
+        # for i in range(n-2, -1, -1):
+        #     rightGre[i] = max(rightGre[i+1], height[i])
 
-        leftGre[0] = height[0]
-        for i in range(1, n):
-            leftGre[i] = max(leftGre[i-1], height[i])
+        # water = 0
+        # for i in range(n):
+        #     water += min(leftGre[i], rightGre[i]) - height[i]
 
-        rightGre[n-1] = height[n-1]
-        for i in range(n-2, -1, -1):
-            rightGre[i] = max(rightGre[i+1], height[i])
+        # return water
 
+
+
+
+
+
+
+
+
+
+
+        left = 0
+        right = len(height) - 1
+
+        leftMax = 0
+        rightMax = 0
         water = 0
-        for i in range(n):
-            water += min(leftGre[i], rightGre[i]) - height[i]
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= leftMax:
+                    leftMax = height[left]
+                else:
+                    water += leftMax - height[left]
+                left += 1
+
+            else:
+                if height[right] >= rightMax:
+                    rightMax = height[right]
+                else:
+                    water += rightMax - height[right]
+                right -= 1
 
         return water
