a0f8b33eee7404d99f3a34c11738eb368c49b773
Rohit <rohitrk.singh1920@gmail.com>
2026-09-21 11:47:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.02%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,5 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # 3 different binary search 1 minvalue in rotated array, then one on left and one on right of that moin value of 1st binary serach
+
+
         # left = 0
         # right = len(nums) - 1
 
@@ -29,8 +32,47 @@ class Solution:
 
 
 
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
 
-        return -1
+        # LineraSearch
+
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         return i
+
+        # return -1
+
+
+        
+        def pivotEle():
+            lo = 0
+            hi = len(nums) - 1
+            while lo < hi:
+                mid = (lo + hi) // 2
+
+                if nums[mid] > nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid
+
+            return lo
+
+        def binarySearch(lo, hi):
+            while lo <= hi:
+                mid = (lo + hi) // 2
+
+                if nums[mid] == target:
+                    return mid
+
+                if nums[mid] < target:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+
+            return -1
+
+        p = pivotEle()
+
+        if nums[p] <= target <= nums[-1]:
+            return binarySearch(p, len(nums) - 1)
+
+        return binarySearch(0, p - 1)
